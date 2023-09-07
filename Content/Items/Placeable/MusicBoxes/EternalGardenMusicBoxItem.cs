74822ba20574742e9966e14c68542334db923365
Dominic <farfetchedking541@gmail.com>
2023-09-06 22:48:14 -0500

    Add Eternal Garden music box

---
+++ Content/Items/Placeable/MusicBoxes/EternalGardenMusicBoxItem.cs
@@ -0,0 +1,29 @@
+﻿using NoxusBoss.Content.Tiles;
+using NoxusBoss.Core.CrossCompatibility;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NoxusBoss.Content.Items.Placeable.MusicBoxes
+{
+    public class EternalGardenMusicBoxItem : ModItem, IToastyQoLChecklistItemSupport
+    {
+        public ToastyQoLRequirement Requirement => ToastyQoLRequirementRegistry.PostNoxus;
+
+        public override void SetStaticDefaults()
+        {
+            // Music boxes can't get prefixes in vanilla.
+            ItemID.Sets.CanGetPrefixes[Type] = false;
+
+            // Recorded music boxes transform into the basic form in shimmer.
+            ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.MusicBox;
+
+            MusicLoader.AddMusicBox(Mod, MusicLoader.GetMusicSlot(Mod, "Assets/Sounds/Music/EternalGarden"), Type, ModContent.TileType<EternalGardenMusicBox>());
+        }
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToMusicBox(ModContent.TileType<EternalGardenMusicBox>());
+        }
+    }
+}
---
+++ Content/Tiles/EternalGardenMusicBox.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+using NoxusBoss.Content.Items.Placeable.MusicBoxes;
+
+namespace NoxusBoss.Content.Tiles
+{
+    public class EternalGardenMusicBox : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileObsidianKill[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.Origin = new Point16(0, 1);
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.newTile.DrawYOffset = 2;
+            TileObjectData.addTile(Type);
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            AddMapEntry(new Color(150, 137, 142));
+        }
+
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+            player.noThrow = 2;
+            player.cursorItemIconEnabled = true;
+            player.cursorItemIconID = ModContent.ItemType<EternalGardenMusicBoxItem>();
+        }
+
+        public override bool CreateDust(int i, int j, ref int type) => false;
+    }
+}
