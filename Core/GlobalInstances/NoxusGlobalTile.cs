4290bb183b19487fde7c93640fbf62ced57e26db
Dominic <farfetchedking541@gmail.com>
2023-09-12 01:03:18 -0500

    Tombstones are now erased in the garden

--- Core/GlobalInstances/NoxusGlobalTile.cs
+++ Core/GlobalInstances/NoxusGlobalTile.cs
@@ -1,10 +1,22 @@
 ﻿using NoxusBoss.Content.Subworlds;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace NoxusBoss.Core.GlobalItems
 {
     public class NoxusGlobalTile : GlobalTile
     {
+        public override void NearbyEffects(int i, int j, int type, bool closer)
+        {
+            if (!EternalGardenUpdateSystem.WasInSubworldLastUpdateFrame)
+                return;
+
+            // Erase tombstones in the garden.
+            if (type == TileID.Tombstones)
+                Main.tile[i, j].Get<TileWallWireStateData>().HasTile = false;
+        }
+
         public static bool IsTileUnbreakable(int x, int y)
         {
             if (EternalGardenUpdateSystem.WasInSubworldLastUpdateFrame)
